72332afa716aec37215f731a17ee782474973828
Luis <alberto.gutierrezcetis138@gmail.com>
2024-07-19 20:20:16 -0600

    Modificación de la comunicación socket, comunicación creada para cada uno de los eventos

--- server.js
+++ server.js
@@ -1,42 +1,61 @@
 const express = require("express");
 const { Server } = require("socket.io");
-const pkg = require("signale");
 const cors = require("cors");
-const dotevn = require("dotenv");
-dotevn.config();
+const dotenv = require("dotenv");
+const pkg = require("signale");
+
+dotenv.config();
 
 const app = express();
-
-const port = process.env.PORT;
-const client = process.env.CLIENT;
+const port = process.env.PORT || 3005;
 const { Signale } = pkg;
 const sigOptions = {
-    secrets: ["([0-9]{4}-?)+"]
+  secrets: ["([0-9]{4}-?)+"]
 };
 const signale = new Signale(sigOptions);
 
 app.use(express.json());
-
-app.use(cors());
+app.use(cors({ origin: "*" }));
 
 const server = app.listen(port, () => {
   signale.success("Socket server running in port: " + port);
 });
 
-const ServerSI = new Server(server, {
+const io = new Server(server, {
   cors: {
     origin: "*",
   },
 });
 
-ServerSI.on("connection", (socket) => {
-  console.log("Connected client");
+io.on("connection", (socket) => {
+  console.log("New client connected");
 
-  socket.on(client, (message) => {
-    ServerSI.emit(client, message);
+  socket.on("bananas", (data) => {
+    // console.log(data);
+    const { date, time, color, classification } = data;
+
+    io.emit("bananas", {
+      date: date,
+      time: time,
+      color: color,
+      classification: classification
+    });
+  });
+
+  socket.on("monitorings", (data) => {
+    // console.log(data);
+    const { date, time, temperature, humidity, weight } = data;
+
+    io.emit("monitorings", {
+      date: date,
+      time: time,
+      temperature: temperature,
+      humidity: humidity,
+      weight: weight
+    });
   });
 
   socket.on("disconnect", () => {
-    console.log("Disconnect client");
+    console.log("Client disconnected");
   });
 });
